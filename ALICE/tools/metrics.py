3dd41ce0840cf4998043b0d89e08ed6ceb6ce9cf
StarLight1212 <102356230+StarLight1212@users.noreply.github.com>
2024-07-18 15:35:17 +0800

    Update metrics.py

--- ALICE/tools/metrics.py
+++ ALICE/tools/metrics.py
@@ -40,8 +40,6 @@ def rmse(y,f):
     return rmse
 
 
-
-
 def pearson(y,f):
     """
     Task:    To compute Pearson correlation coefficient
@@ -57,8 +55,6 @@ def pearson(y,f):
     return rp
 
 
-
-
 def spearman(y,f):
     """
     Task:    To compute Spearman's rank correlation coefficient
@@ -72,8 +68,6 @@ def spearman(y,f):
     rs = stats.spearmanr(y, f)[0]
 
     return rs
-
-
 
 
 def ci(y,f):
@@ -117,8 +111,6 @@ def ci(y,f):
     ci = S/z
 
     return ci
-
-
 
 
 def f1(y,f):
@@ -182,37 +174,5 @@ def MedAE(y, pred):
     return median_absolute_error(y, pred)
 
 
-
 def class_scores(label, pred):
     return auc(label, pred)
-
-
-def average_AUC(y,f):
-
-    """
-    Task:    To compute average area under the ROC curves (AUC) given ten
-             interaction threshold values from the pKd interval [6 M, 8 M]
-             to binarize pKd's into true class labels.
-
-    Input:   y      Vector with original labels (pKd [M])
-             f      Vector with predicted labels (pKd [M])
-
-    Output:  avAUC   average AUC
-
-    """
-
-    thr = np.linspace(6,8,10)
-    auc = np.empty(np.shape(thr)); auc[:] = np.nan
-
-    for i in range(len(thr)):
-        y_binary = copy.deepcopy(y)
-        y_binary = preprocessing.binarize(y_binary.values.reshape(1,-1), threshold=thr[i], copy=False)[0]
-        fpr, tpr, thresholds = metrics.roc_curve(y_binary, f, pos_label=1)
-        auc[i] = metrics.auc(fpr, tpr)
-
-    avAUC = np.mean(auc)
-
-    return avAUC
-
-
-
